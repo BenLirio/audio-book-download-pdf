196063d6c1ed4ab29f927b6a24d546690c3971a4
BenLirio <benjlirio@gmail.com>
2020-06-11 17:12:01 -0400

    add public files

--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
+const multer = require("multer");
 const app = express();
+const upload = multer({ dest: "uploads/" });
+
+app.use(express.static(__dirname + "/public"));
 
 app.use("/", (req, res) => {
-  res.send("test");
+  res.sendfile(__dirname + "/public/index.html");
+});
+
+app.post("/profile", upload.single("avatar"), (req, res, next) => {
+  console.log(req.file);
 });
 
 app.listen(8080, () => {
